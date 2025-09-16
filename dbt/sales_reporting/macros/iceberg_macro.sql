af06145a0cfe0bc9a7b304aa79d3141b6087d52d
kmbeyond <kiran.learn7@gmail.com>
2025-09-16 12:38:29 -0500

    Update iceberg_macro

--- dbt/sales_reporting/macros/iceberg_macro.sql
+++ dbt/sales_reporting/macros/iceberg_macro.sql
@@ -1,45 +1,40 @@
 {% macro get_incremental_iceberg_merge_sql(arg_dict) %}
 
-  {% set dest_columns = adapter.get_columns_in_relation(this) %}
-  {% if dest_columns | length == 0 %}
-     {% set dest_columns = adapter.get_columns_in_relation(arg_dict["temp_relation"]) %}
-  {% endif %}
+ {% set target = arg_dict["target_relation"] %}
+ {% set source = arg_dict["temp_relation"] %}
+ {% set unique_key = arg_dict["unique_key"] %}
+ {% set incremental_predicates = arg_dict["incremental_predicates"] %}
 
-  {% do return(my_custom_merge_sql(arg_dict["target_relation"], arg_dict["temp_relation"], arg_dict["unique_key"], dest_columns, arg_dict["incremental_predicates"])) %}
+ {{ sync_columns(source, target) }}
 
-{% endmacro %}
-
-
-
-{% macro my_custom_merge_sql(target, source, unique_key, dest_columns, incremental_predicates) %}
-
-{%- set dest_join_filter -%}
+ {%- set dest_join_filter -%}
     {%- for col in unique_key -%}
         DBT_INTERNAL_DEST.{{ col }} = DBT_INTERNAL_SOURCE.{{ col }}{% if not loop.last %} AND {% endif %}
     {%- endfor -%}
-{%- endset -%}
+ {%- endset -%}
 
-{%- set dest_cols_update -%}
-    {%- for col in dest_columns | map(attribute="name") -%}
+ {% set source_columns = adapter.get_columns_in_relation(source) %}
+ {%- set dest_cols_update -%}
+    {%- for col in source_columns | map(attribute="name") -%}
         {{ col }} = DBT_INTERNAL_SOURCE.{{ col }}{% if not loop.last %}, {% endif %}
     {%- endfor -%}
-{%- endset -%}
+ {%- endset -%}
 
-{%- set dest_col_names -%}
-    {%- for col in dest_columns | map(attribute="name") -%}
+ {%- set dest_col_names -%}
+    {%- for col in source_columns | map(attribute="name") -%}
         {{ col }}{% if not loop.last %}, {% endif %}
     {%- endfor -%}
-{%- endset -%}
+ {%- endset -%}
 
-{%- set dest_cols_insert_values -%}
-    {%- for col in dest_columns | map(attribute="name") -%}
+ {%- set dest_cols_insert_values -%}
+    {%- for col in source_columns | map(attribute="name") -%}
         DBT_INTERNAL_SOURCE.{{ col }}{% if not loop.last %}, {% endif %}
     {%- endfor -%}
-{%- endset -%}
+ {%- endset -%}
 
 
-    MERGE INTO {{ target }} AS DBT_INTERNAL_DEST
-    USING {{ source }} AS DBT_INTERNAL_SOURCE
+ MERGE INTO {{ target }} AS DBT_INTERNAL_DEST
+  USING {{ source }} AS DBT_INTERNAL_SOURCE
       ON {{ dest_join_filter }}
     WHEN MATCHED THEN UPDATE SET
         {{ dest_cols_update }}
